d2cd54edadb42dca4bc1c40d984bd11e373dfabf
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-09-28 22:29:35 +0700

    lecture - type and variable

---
+++ sources/variable.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    // declare
+    int position;
+    int x, y, z;
+    x = 1;
+    y = 1;
+    z = 1;
+
+    float myNum = 100.60;
+
+    double myDouble = 100.123456;
+
+    char c = 'Y';
+    char c2 = 113;
+
+    // assign 
+    position = 10;
+
+    printf("Position: %d\n", position);
+    printf("x: %d, y: %d, z: %d\n", x, y, z);
+    printf("Mynum: %f\n", myNum);
+    printf("MyDouble: %lf\n", myDouble);
+    printf("c: %c\n", c);
+    printf("c2: %c\n", c2);
+
+    return 0;
+}
