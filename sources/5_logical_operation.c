e6776dd386a3ab61194c5b3288e1301f74e67ab8
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-10-05 14:15:00 +0700

    lecture - logical statement

--- sources/5_logical_operation.c
+++ sources/5_logical_operation.c
@@ -2,5 +2,35 @@
 
 int main()
 {
+    int year = 2020;
+    int price = 21000;
+    
+    if (year >= 2019 && price <= 20000)
+    {
+        printf("AND Ok!\n");
+    }
+    else 
+    {
+        printf("AND Not ok\n");
+    }
+
+    if (year >= 2019 || price <= 20000)
+    {
+        printf("OR OK\n");
+    }
+    else 
+    {
+        printf("OR Not ok!\n");
+    }
+
+    if (!(year >= 2019))
+    {
+        printf("NOT ok!\n");
+    }
+    else 
+    {
+        printf("NOT Not ok!\n");
+    }
+
     return 0;
 }
