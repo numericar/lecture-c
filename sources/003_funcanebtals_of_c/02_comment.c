50335959db119f6d82d1b0c1e1d0bbf3cafb7ecd
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-08-10 10:43:10 +0700

    lecture: comment

---
+++ sources/003_funcanebtals_of_c/02_comment.c
@@ -0,0 +1,14 @@
+/* 
+    Wtritten by Numericar
+    Copyright 2025
+*/
+
+#include <stdio.h>
+
+// This code is display my name :)
+int main(void) 
+{
+    printf("My name is Chanokchon");
+    
+    return 0;
+}
